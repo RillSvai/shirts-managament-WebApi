3ea5a438273544929e0668fd08331f16238c25a7
RillSvai <kirilkvas07@gmail.com>
2023-11-26 13:12:55 +0200

    added web consumer

---
+++ ShirtsManagament.Web/Controllers/ShirtController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShirtsManagament.Web.Controllers;
+
+public class ShirtController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
