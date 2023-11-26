1650de162d685d7aea0d897e8f6785cf1072cbf7
RillSvai <kirilkvas07@gmail.com>
2023-11-26 14:09:50 +0200

    added

---
+++ ShirtsManagement.Web/Controllers/ShirtController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShirtsManagement.Web.Controllers;
+
+public class ShirtController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
