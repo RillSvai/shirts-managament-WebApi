ac89d18f9ffc288d692473d2f7d27fadc13df5a3
RillSvai <kirilkvas07@gmail.com>
2023-11-26 13:40:55 +0200

    added web consumet in third time(

--- ShirtsManagament.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ShirtsManagament.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ ShirtsManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShirtsManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
